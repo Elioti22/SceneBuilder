b1c0816d81311ed074aa5dd06862e25cc6652f61
Eliot Martinez <eliot@MacBook-Air-de-Eliot.local>
2023-01-12 19:33:06 +0100

    plateau pour 7 joueurs

--- src/main/java/com/example/scenebuilder/HelloApplication.java
+++ src/main/java/com/example/scenebuilder/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader0 = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene0 = new Scene(fxmlLoader0.load(), 629, 783);
+        FXMLLoader fxmlLoader0 = new FXMLLoader(HelloApplication.class.getResource("plateau7joueurs.fxml"));
+        Scene scene0 = new Scene(fxmlLoader0.load(), 850, 903);
         stage.setTitle("Chargement ...");
         stage.setScene(scene0);
         stage.show();
